94fb96617246317116ea3f25d9878fb732735322
James Ding <james.ding@traivefinance.com>
2021-10-11 11:05:48 -0500

    file renamed

--- routes/base.js
+++ routes/base.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require('express');
 const app = express();
-const { home, health, stats } = require('../services/baseService');
+const { home, health, stats } = require('../services/base');
 
 app.get('/', home);
 app.get('/health', health);
